cccf8b455cc5e01364146d3187ab84371a31f272
Oyinda David <oyindadavid18@gmail.com>
2021-01-18 14:39:17 +0100

    remove 2020 from list of years

--- netflix.py
+++ netflix.py
@@ -25,12 +25,15 @@ countries_data = countries_with_most_titles(df, 'country', 11)
 countries_df = pd.DataFrame(list(countries_data.items()), columns = ['country','no_of_titles']) 
 countries_df = countries_df.drop(countries_df.index[2])
 
+# remove 2020 from list of years
+list_of_years = list(reversed(sorted(df.release_year.unique())))
+store_2020 = list_of_years.pop(0)
+
+
 st.sidebar.header('User Input Features')
 selected_type = st.sidebar.selectbox('Filter by Type', df.type.unique())
 selected_category = st.sidebar.selectbox('Filter by Category', df.listed_in.unique())
-selected_year = st.sidebar.selectbox('Filter by Year', list(reversed(sorted(df.release_year.unique()))))
-
-
+selected_year = st.sidebar.selectbox('Filter by Year', list_of_years)
 
 # Filtering data
 df_selected_show = df[(df.release_year==selected_year) & (df.type==selected_type) & (df.listed_in==selected_category)]
